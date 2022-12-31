8749b2427cd37abfd7313d1797afe788646a08d5
Romero <romero@MacBook-Pro-de-Romero.local>
2022-12-31 15:11:18 +0100

    change questionForm

--- tp3-flutter-firebase/lib/bloc_cubit/presentation/pages/question_creation_page.dart
+++ tp3-flutter-firebase/lib/bloc_cubit/presentation/pages/question_creation_page.dart
@@ -18,18 +18,29 @@ class QuestionCreationPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('TP3 - Question Creation')),
-      body: Form(
-          key: _formKey,
-          child: Column(
-            children: [
-              textField(const Icon(Icons.question_mark), "Question text", setQuestionText),
-              textField(const Icon(Icons.attach_file), "Url image", setUrlImage),
-              textField(const Icon(Icons.done), "Correct answer (0 ou 1)", setCorrect),
-              textField(const Icon(Icons.text_fields), "Answer text", setAnswerText),
-              textField(const Icon(Icons.topic), "Thematic", setThematic),
-              submitButton(context),
-            ],
-          )),
+      body: Center(
+        child: SizedBox(
+          width: 400,
+          child: Card(
+            child: Form(
+                key: _formKey,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      textField(const Icon(Icons.question_mark), "Question text", setQuestionText),
+                      textField(const Icon(Icons.attach_file), "Url image", setUrlImage),
+                      textField(const Icon(Icons.done), "Correct answer (0 ou 1)", setCorrect),
+                      textField(const Icon(Icons.text_fields), "Answer text", setAnswerText),
+                      textField(const Icon(Icons.topic), "Thematic", setThematic),
+                      submitButton(context),
+                    ],
+                  ),
+                )),
+          ),
+        ),
+      ),
     );
   }
 
@@ -51,18 +62,22 @@ class QuestionCreationPage extends StatelessWidget {
     );
   }
 
-  TextFormField textField(Icon icon, String labelText, Function onSavedFunction) {
-    return TextFormField(
-        decoration: InputDecoration(icon: icon, labelText: labelText),
-        validator: (value) {
-          if (value == null || value.isEmpty) {
-            return 'Please enter some text';
-          }
-          return null;
-        },
-        onSaved: (value) {
-          onSavedFunction(value);
-        });
+  Padding textField(Icon icon, String labelText, Function onSavedFunction) {
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: TextFormField(
+          decoration: InputDecoration(prefixIcon: icon, labelText: labelText, border: const OutlineInputBorder()),
+          style: const TextStyle(fontSize: 15),
+          validator: (value) {
+            if (value == null || value.isEmpty) {
+              return 'Please enter some text';
+            }
+            return null;
+          },
+          onSaved: (value) {
+            onSavedFunction(value);
+          }),
+    );
   }
 
   void setQuestionText(String text) {
